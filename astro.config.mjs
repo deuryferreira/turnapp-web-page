656f257976604164000642a2b73f757ad9e2350c
deuryferreira <deury.fr@gmail.com>
2026-06-04 11:37:04 -0400

    a

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import vercel from "@astrojs/vercel";
 
 export default defineConfig({
   site: "https://turnapp.ferreiraric.com",
-  output: "server",
+  output: "static",
   outDir: "build",
   prefetch: true,
 
