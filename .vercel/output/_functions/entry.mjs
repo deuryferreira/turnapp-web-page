8e2e20171367ed81737af4fa1a9ab37ac8853f2c
deuryferreira <deury.fr@gmail.com>
2026-06-04 02:57:41 -0400

    fixed2

---
+++ .vercel/output/_functions/entry.mjs
@@ -0,0 +1,1 @@
+export { B as default } from './chunks/entrypoint_C6Jj0f0Y.mjs';
--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel";
 
 export default defineConfig({
   site: "https://turnapp.ferreiraric.com",
